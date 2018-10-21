3ff0c180629d7d88ead7566f7a8c60a0e2a6d81e
Anna Lára <annasig95@gmail.com>
2018-10-21 12:37:31 +0000

    creating board and tested, paired with Olafur

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,1 +1,12 @@
-//ticTacToe.js
+//TicTacToe.js
+function initializeBoard(){
+
+	var board = [];
+
+	for(i = 0; i < 9; i++){
+		board[i] = parseInt(i);
+	}
+	return board[2];
+}
+
+module.exports = initializeBoard;
--- TicTacToe.test.js
+++ TicTacToe.test.js
@@ -1,1 +1,8 @@
 //TicTacToe.test.js
+
+const initializeBoard = require('./TicTacToe');
+
+it("Should return the number 2 if board is initalized correct", () => {
+	expect(initializeBoard()).toBe(2);
+});
+
