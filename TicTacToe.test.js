06da624a2a6be2f39ef258ce273f4fca1c9a4517
Anna Lára <annasig95@gmail.com>
2018-10-24 16:12:12 +0000

    Delete all changes to the files, try again

--- TicTacToe.test.js
+++
@@ -1,8 +0,0 @@
-//TicTacToe.test.js
-
-const initializeBoard = require('./TicTacToe');
-
-it("Should return the number 2 if board is initalized correct", () => {
-	expect(initializeBoard()).toBe(2);
-});
-
